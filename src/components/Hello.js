ef7a0d2ceb3b1a6187bde8291b1379626b208be8
Hobbes <chilee83@gmail.com>
2019-06-03 16:33:15 -0400

    Initial page navbar and jumbotron tested

---
+++ src/components/Hello.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Hello = () => {
+    const name = "Chi";
+    return(
+        <div>
+            <h1>Hello, my name is {name}</h1>
+            <h2>This page is a test</h2>
+       
+        </div>
+    );
+};
+   
+export default Hello;
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Nav = () => {
+    return (
+        <nav className="navbar navbar-light bg-light">
+          <a className="navbar-brand" href="#">
+            Clicky-Game
+          </a>
+        </nav>
+    );
+};
+
+export default Nav;
