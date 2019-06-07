ee8441f8aaf52151f4377d3098cdc12b74cd224e
Hobbes <chilee83@gmail.com>
2019-06-07 12:18:55 -0400

    Updates

---
+++ precache-manifest.e2096431d48be0e47e6dd0b8c0d770ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0aa8ae6f92c649eb376adcc1b001039",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "612633f2f67bc30ec0f9",
+    "url": "/clicky-game/static/css/main.d22d584e.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/clicky-game/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "612633f2f67bc30ec0f9",
+    "url": "/clicky-game/static/js/main.5558f51a.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
