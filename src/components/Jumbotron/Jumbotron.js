1c654efa588da4954dc2a2c2488d6bcbd5ca36ad
Hobbes <chilee83@gmail.com>
2019-06-07 14:53:28 -0400

    updated game info

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -8,9 +8,7 @@ const Jumbotron = () => {
             <div className="col-12 text-center">
             <div className="display-3 bolder">Welcome to the Clicky Game!</div><br></br>
             <h4>
-                This is a memory game. You have to click on one image at a time.
-                Each time you click on a new image, you will gain one point. 
-                If you click on an image you already clicked on, you will lose. 
+                This is a memory game. 
                 The goal is to click on all 16 images only once!
             </h4>
             <h4>Good Luck!</h4>
